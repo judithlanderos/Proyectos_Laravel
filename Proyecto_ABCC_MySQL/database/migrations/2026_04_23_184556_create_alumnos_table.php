c0bf8ccef5c6511250ebfd27c0927e2b5c6bd7b8
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-09 00:44:07 -0600

    Commit migraciones

--- Proyecto_ABCC_MySQL/database/migrations/2026_04_23_184556_create_alumnos_table.php
+++ Proyecto_ABCC_MySQL/database/migrations/2026_04_23_184556_create_alumnos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('alumnos', function (Blueprint $table) {
+        Schema::createIfNotExists('alumnos', function (Blueprint $table) {
             $table->id();
             $table->string("Num_Control")->unique();
             $table->string("Nombre");
--- Proyecto_ABCC_MySQL/database/migrations/2026_05_09_043952_create_usuarios_table.php
+++ Proyecto_ABCC_MySQL/database/migrations/2026_05_09_043952_create_usuarios_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        Schema::createIfNotExists('usuarios', function (Blueprint $table) {
         $table->id();
         $table->string('nombre');
         $table->string('correo')->unique();
